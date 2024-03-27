92ddaf95c286c5a6541a50a4cd4e7da8b71b7804
Abhishek-Gurav <agurav066@gmail.com>
2024-03-27 21:07:02 +0530

    removed extra things

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import WeatherDisplay from "./components/WeatherDisplay";
 import clouds from "./images/fewclouds.webp";
