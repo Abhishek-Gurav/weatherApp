709c3784a9c755137f8ea0a12532e39a8b1c72c3
Abhishek-Gurav <agurav066@gmail.com>
2024-03-27 20:53:45 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherDisplay from "./components/WeatherDisplay";
+import clouds from "./images/fewclouds.webp";
+import clearsky from "./images/clearsky.webp";
+import thunderstorm from "./images/thunderstorm.webp";
+import snow from "./images/snow.jpeg";
+import showerrain from "./images/showerrain.jpeg";
+import mist from "./images/mist.webp";
 import "./styles.css";
 const WeatherApp = () => {
   const [cityName, setCityName] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
-  const [selectedOption, setSelectedOption] = useState("cityName");
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
 
-  const handleTabClick = (option) => {
-    setSelectedOption(option);
-  };
 
   const appId = "0f871056c1dab081a447965f8702e88a"; // Your OpenWeatherMap API Key
 
@@ -28,150 +28,100 @@ const WeatherApp = () => {
   };
 
   const handleCityNameSubmit = (e) => {
-    e.preventDefault();
+    console.log(cityName)
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appId}&units=metric`;
-    fetchWeatherData(url);
-  };
-
-  const handleCoordinatesSubmit = (e) => {
-    e.preventDefault();
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appId}&units=metric`;
     fetchWeatherData(url);
   };
 
   const handleLocationSubmit = async (e) => {
     e.preventDefault();
     try {
-       // Request permission to access location
-       const position = await new Promise((resolve, reject) => {
-         navigator.geolocation.getCurrentPosition(resolve, reject);
-       });
-       console.log(position)
-       const { latitude, longitude } = position.coords;
-   
-       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appId}&units=metric`;
+      // Request permission to access location
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      console.log(position);
+      const { latitude, longitude } = position.coords;
 
-       fetchWeatherData(url);
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appId}&units=metric`;
+
+      fetchWeatherData(url);
     } catch (error) {
-       // Handle errors, such as when the user denies location access
-       setError("Error accessing location. Please allow location access.");
+      // Handle errors, such as when the user denies location access
+      setError("Error accessing location. Please allow location access.");
     }
-   };
+  };
+  const condition = weatherData?.weather[0].main;
+  const setBackgroundImage = () => {
+    switch (condition) {
+      case "Clouds":
+        return clouds;
+      case "Clear":
+        return clearsky;
+      case "Thunderstorm":
+        return thunderstorm;
+      case "Snow":
+        return snow;
+      case "Rain":
+        return showerrain;
+      case "Atmosphere":
+        return mist;
+      default:
+        return mist;
+    }
+  };
 
+  useEffect(() => {
+    handleCityNameSubmit()
+  }, [cityName])
+  
   return (
-  <div className="h-screen bg-cover bg-fixed bg-center">
-      <nav className="flex items-center justify-between p-4 bg-black bg-opacity-75">
-        <a className="text-3xl font-bold text-green-400" href="/">Weather</a>
-      </nav>
-    <div className="lg:px-20 px-5">
-      <div className="tab-container lg:w-8/12 mx-auto grid grid-cols-3 my-3">
-        <button
-          style={
-            selectedOption === "cityName"
-              ? { fontWeight: "bold", borderBottom: "2px solid black" }
-              : { borderBottom: "2px solid #F5F7F8" }
-          }
-          className="w-full"
-          onClick={() => handleTabClick("cityName")}
-        >
-          City Name
-        </button>
-
-        <button
-          style={
-            selectedOption === "coordinates"
-              ? { fontWeight: "bold", borderBottom: "2px solid black" }
-              : { borderBottom: "2px solid #F5F7F8" }
-          }
-          className="w-full"
-          onClick={() => handleTabClick("coordinates")}
-        >
-          Co-Ordinates
-        </button>
-
-        <button
-          style={
-            selectedOption === "location"
-              ? { fontWeight: "bold", borderBottom: "2px solid black" }
-              : { borderBottom: "2px solid #F5F7F8" }
-          }
-          className="w-full"
-          onClick={() => handleTabClick("location")}
-        >
-          Location
-        </button>
-      </div>
-      <div className="container mx-auto flex justify-center items-center h-full">
-     <div className="bg-white bg-opacity-75 p-8 rounded-lg text-center">
-        {selectedOption === "cityName" && (
-          <>
-          <h1 className="text-green-500 text-4xl font-bold mb-8">Check Weather</h1>
-          <form onSubmit={handleCityNameSubmit}>
-            <input
-              type="text"
-              value={cityName}
-              className="w-full border-2 px-1 py-0.5 my-2 rounded"
-              onChange={(e) => setCityName(e.target.value)}
-              placeholder="Enter city name"
-            />
-            <button
-              className="w-full bg-black text-white py-0.5 rounded"
-              type="submit"
-            >
-              Get Weather
-            </button>
-          </form>
-          </>
-        )}
-
-        {selectedOption === "coordinates" && (
-          <>
-          <h1 className="text-green-500 text-4xl font-bold mb-8">Check Weather</h1>
-          <form onSubmit={handleCoordinatesSubmit}>
-            <input
-              type="text"
-              value={latitude}
-              className="w-full border-2 px-1 py-0.5 my-2 rounded"
-              onChange={(e) => setLatitude(e.target.value)}
-              placeholder="Enter latitude"
-            />
-            <input
-              type="text"
-              value={longitude}
-              className="w-full border-2 px-1 py-0.5 my-2 rounded"
-              onChange={(e) => setLongitude(e.target.value)}
-              placeholder="Enter longitude"
-            />
-            <button
-              className="w-full py-0.5 rounded bg-black text-white"
-              type="submit"
-            >
-              Get Weather
-            </button>
-          </form>
-          </>
-        )}
-
-        {selectedOption === "location" && (
-          <>
-          <h1 className="text-green-500 text-4xl font-bold mb-8">Check Weather</h1>
-          <form onSubmit={handleLocationSubmit}>
-          <button
-              className="w-full py-0.5 rounded bg-black text-white"
-              type="submit"
-            >
-              Get Weather
-            </button>
-          </form>
-          </>
-        )}
+    <div
+      style={{
+        backgroundImage: `url(${setBackgroundImage()})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        minHeight: "100vh"
+      }}
+      className=" flex flex-col justify-start items-center"
+    >
+      <div className="lg:px-20 px-5">
+        <div className="container mx-auto flex justify-center items-center">
+          <div className="p-8 rounded-lg text-center flex">
+                  <input
+                    type="text"
+                    value={cityName}
+                    className="w-full px-1 py-0.5 my-2"
+                    onChange={(e) => {
+                      setCityName(e.target.value);
+                    }}
+                    placeholder="Enter city name"
+                  />
+                  <i onClick={handleLocationSubmit} className="material-icons text-4xl">location_on</i>
+          </div>
         </div>
+        {error && <p>{error}</p>}
+        {weatherData && <WeatherDisplay weatherData={weatherData} />}
       </div>
-      {error && <p>{error}</p>}
-      {weatherData && <WeatherDisplay weatherData={weatherData} />}
-    </div>
     </div>
   );
 };
 
 export default WeatherApp;
+
+// {selectedOption === "location" && (
+//               <>
+//                 <h1 className="text-green-500 text-4xl font-bold mb-8">
+//                   Check Weather
+//                 </h1>
+//                 <form onSubmit={handleLocationSubmit}>
+//                   <button
+//                     className="w-full py-0.5 rounded bg-black text-white"
+//                     type="submit"
+//                   >
+//                     Get Weather
+//                   </button>
+//                 </form>
+//               </>
+//             )} 
--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { Typography, Row, Col, Progress } from "antd";
-import clouds from "../images/fewclouds.webp";
-import clearsky from "../images/clearsky.webp";
-import thunderstorm from "../images/thunderstorm.webp";
-import snow from "../images/snow.jpeg";
-import showerrain from "../images/showerrain.jpeg";
-import mist from "../images/mist.webp";
+import { Typography, Progress } from "antd";
 import "../styles.css";
 
 const { Title } = Typography;
@@ -16,91 +10,50 @@ const WeatherInfo = ({ weatherData }) => {
   const humidity = weatherData.main.humidity;
   const pressure = weatherData.main.pressure;
   const windSpeed = weatherData.wind.speed;
-  const cityName = weatherData.name;
-  const country = weatherData.sys.country;
-  const condition = weatherData.weather[0].main;
 
   const imgUrl = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-
-  // Function to set icon based on condition
-  const setBackgroundImage = () => {
-    switch (condition) {
-      case "Clouds":
-        return clouds;
-      case "Clear":
-        return clearsky;
-      case "Thunderstorm":
-        return thunderstorm;
-      case "Snow":
-        return snow;
-      case "Rain":
-        return showerrain;
-      case "Atmosphere":
-        return mist;
-      default:
-        return mist;
-    }
-  };
 
   return (
     <>
       <div
-        id="top"
-        className="w-full flex flex-row justify-around items-center"
-        style={{
-          backgroundImage: `url(${setBackgroundImage()})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center center",
-          minHeight: "15vw",
-        }}
-      >
-        <Row className="w-full flex flex-row justify-around items-center">
-          <Col>
-            <Title level={1} style={{ color: "white" }}>
-              {temp}
-              <sup style={{ fontSize: "1.5rem" }}>°C</sup>
-            </Title>
-          </Col>
-          <Col>
-            <div>
-              <Title level={3} style={{ color: "white" }}>
-                {cityName}
-              </Title>
-              <Title level={3} style={{ color: "white" }}>
-                {country}
-              </Title>
-              <Title level={3} style={{ color: "white" }}>
-                {condition}
-              </Title>
-            </div>
-          </Col>
-        </Row>
-      </div>
-      <div
         className="w-full mt-10 p-10 flex flex-wrap gap-16 justify-around items-center text-center"
         style={{
-          backgroundColor: "#DAEAF1",
+          background: "rgba(255, 255, 255, 0.2)",
+          borderRadius: "16px",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(5px)",
+          border: "1px solid rgba(255, 255, 255, 0.3)",
         }}
       >
-        <div className="one">
-          <img src={imgUrl} width="100%" alt="" />
-          <b className="text-lg">{weatherDescription}</b>
+        <div id="top" className="flex justify-around items-center">
+          <div className="one">
+          <div className="w-full flex justify-center">
+            <img src={imgUrl} className="w-3/6 sm:w-full" alt="" />
+            </div>
+            <b className="text-sm sm:text-lg">{weatherDescription}</b>
+          </div>
+          <div className="text-sm sm:text-3xl absolute right-2 top-2" style={{ color: "white" }}>
+            {temp}
+            <sup>°C</sup>
+          </div>
+          <div></div>
         </div>
-        <div className="one">
-          <i className="material-icons text-4xl">opacity</i>
-          <h3 className="text-lg">Humidity</h3>
-          <Progress type="circle"  percent={humidity} size="small" />
-        </div>
-        <div className="one">
-          <i className="material-icons text-4xl">insights</i>
-          <h3 className="text-lg">Pressure</h3>
-          <h2 className="text-lg">{pressure}</h2>
-        </div>
-        <div className="one">
-          <i className="material-icons text-4xl">air</i>
-          <h3 className="text-lg">Wind Speed</h3>
-          <h2 className="text-lg">{windSpeed}</h2>
+        <div className="flex flex-col gap-10 sm:flex-row justify-around w-full">
+          <div className="flex flex-col justify-around">
+            <i className="material-icons text-4sm">opacity</i>
+            <h3 className="text-sm lg:text-lg">Humidity</h3>
+            <Progress strokeColor={"#fff"} type="circle" percent={humidity} size="small" />
+          </div>
+          <div className="one">
+            <i className="material-icons text-4sm">insights</i>
+            <h3 className="text-sm lg:text-lg">Pressure</h3>
+            <h2 className="text-sm lg:text-lg">{pressure}</h2>
+          </div>
+          <div className="one">
+            <i className="material-icons text-4sm">air</i>
+            <h3 className="text-sm lg:text-lg">Wind Speed</h3>
+            <h2 className="text-sm lg:text-lg">{windSpeed}</h2>
+          </div>
         </div>
       </div>
     </>
